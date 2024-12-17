97ba25da3caf79d78cd888d88bdcc3d4d19d0872
Nicholas Hanks <104238034+NicHanks@users.noreply.github.com>
2024-12-17 15:08:43 -0700

    trying to work on graphing

---
+++ notes.py
@@ -0,0 +1,15 @@
+
+#wtfrick is workspace folder "save to workplaces folder???"
+
+"""
+TERMINAL:
+python3 -m venv meat env
+source meat_env/bin/activate | /users/nicholas/documents/meat/meat_env/bin/activate
+pip install numpy pandas jupyter notebook ipykernel matplotlib
+#pip: 24.2 -> 24.3.1/pip install --upgrade pip | 12/9/2024
+
+IF JUPYTER:
+python3 -m ipykernel install --user --name=meat_env --display-name "Python (meat_env)" #connect py
+python3 -m ipykernel --user --name something --name-shown "name (ipykernel)" #general purpose
+~/.local/share/jupyter/kernels #jupyter's configuratory files.
+"""
--- time_calc.py
+++ time_calc.py
@@ -2,74 +2,111 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+# print(plt.__version__)
 
-
-
-import tkinter 
-from tkinter.constants import * 
-tk = tkinter.Tk() 
-frame = tkinter.Frame(tk, relief=RIDGE, borderwidth=2) 
-frame.pack(fill=BOTH,expand=1) 
-label = tkinter.Label(frame, text="Hello, World") 
-label.pack(fill=X, expand=1) 
-button = tkinter.Button(frame,text="Exit",command=tk.destroy) 
-button.pack(side=BOTTOM) 
-tk.mainloop()
+# import tkinter 
+# from tkinter.constants import * #might  use tkinter, need it for the buttons... (eventually)
 
 
 # MEAT cutting time calculator
 """The purpose of this program is to calculate the time it takes to execute a certain number of cuts, and track progress towards next quarter's quota."""
 
-#times it takes to make each cut.
 times = { # nameof cut : [time it takes to make each cut(in sec)]
     "Create_New" : [],
     "Trachea Cut" : [3],
-    "Stomache Cut" : [5.5]
-}
+    "Stomach Cut" : [5.5]}
+ed_g = 75
+
+
+dt_ed = [103.07, 98.52, 99.03, 100.71, 95.41, 97.50, 96.55, 96.77, 96.20, 94.25, 106.41, 100.92, 93.44, 95.34, 108.47, 95.71, 103.84, 99.99, 99.52, 96.87, 106.39, 100.36, 99.50, 95.27, 98.13, 101.03, 103.83, 96.06, 99.45, 98.56, 99.52, 103.51, 97.21, 98.28, 92.82, 96.52, 100.63, 110.46]  # Daily total edible
+rt_ed = np.cumsum(dt_ed)
+
+dt_id = [ 89.59, 91.26, 93.76, 84.51, 90.36, 87.91, 80.75, 83.34, 84.06, 78.26, 79.23, 86.48, 86.04, 85.69, 81.3, 85.66, 89.02, 84.53, 85.49, 103.45, 80.81, 84.24, 92.75, 84.24, 95.95, 87.53, 84.47, 99.21, 90.92, 84.19, 83.56, 81.85, 90.09, 88.12, 84.54, 89.06, 88.76, 93.91] # Daily total in-dedible
+rt_id = np.cumsum(dt_id)
+
+
+#RAW DATA:
+"""
+dt_ed_RAW = [103.07, 98.52, 99.03, 100.71, 95.41, 97.50, 96.55,, 96.77, 96.20, 94.25, 106.41, 100.92, 93.44, 95.34, 108.47, 95.71, 103.84, NA, 99.99, 99.52, 96.87, 106.39, 100.36, 99.50, 95.27, 98.13, 101.03, 103.83, 96.06, 99.45, 98.56, 99.52, 103.51, 97.21, 98.28, 92.82, 96.52, 100.63, 110.46]
+dt_id_RAW = [, 89.59, 91.26, 93.76, 84.51, 90.36, 87.91, 80.75, 83.34, 84.06, 78.26, 79.23, 86.48, 86.04, 85.69, 81.3, 85.66, 89.02, NA, 84.53, 85.49, 103.45, 80.81, 84.24, 92.75, 84.24, 95.95, 87.53, 84.47, 99.21, 90.92, 84.19, 83.56, 81.85, 90.09, 88.12, 84.54, 89.06, 88.76, 93.91]
+#PROPER WAY OF DOING IT: 
+#OPTION 1:
+indices_na = np.where(dt_id_array == 'NA')[0]
+dt_id_array[indices_na] = np.nan
+#OPTION 2:
+dt_id_series = pd.Series(dt_id_RAW) 
+dt_id_series = dt_id_series.replace('NA', np.nan) #better
+#THEN
+dt_ed_RAW = list(filter(None, dt_ed_RAW)) #gets rid of extra gaps/spaces, or commas
+#OR
+dt_ed_RAW = [x for x in dt_ed_RAW if x] #gets rid of extra gaps/spaces, or commas
+
+dt_id_raw = np.array([89.59, 91.26, 93.76, 84.51, 90.36, 87.91, 80.75, 83.34, 84.06, 78.26, 79.23, 86.48, 86.04, 85.69, 81.3, 85.66, 89.02, np.nan, 84.53, 85.49, 103.45, 80.81, 84.24, 92.75, 84.24, 95.95, 87.53, 84.47, 99.21, 90.92, 84.19, 83.56, 81.85, 90.09, 88.12, 84.54, 89.06, 88.76, 93.91])
+
+# Handle missing values (replace with a specific value or remove)
+dt_id_cleaned = np.nan_to_num(dt_id_raw, nan=0)  # Replace missing values with 0
+# Alternatively, you can remove missing values:
+# dt_id_cleaned = dt_id_raw[~np.isnan(dt_id_raw)]
+"""
+
+def addrt(ed,id): #rtet = running total edible tallow
+    global rt_ed #why is this not blue????
+    global rt_id
+    rt_ed += ed
+    rt_id += id
+
+# def daily_goals(et=75,it=80): #pph_et_g = pounds per head edible tallow goal
+#     global ed_g
+#     global id_g
+#     ed_g = et
+#     id_g = it
+ed_g = 75
+id_g = 80
+
+def quartly_goals(d=62): #d=day(s) 
+    global ed_qg
+    global id_qg
+    ed_qg = ed_g * d
+    id_qg = id_g * d
 
 def menu():
     """Displays menu."""
 
-    input = input(print("""\n
-    Menu: \n
-    Time Calculator (1) \n
-    Tracker (2)\n
-    Punch in (3)\n
-    Punch out (4)\n
-    Creat a new cut (name of the cut and the time needed to make the cut is required) (5) \n
-    """))
-    if input == 1:
+    m_in = input(print("""
+    Menu: 
+    Time Calculator (1) 
+    Tracker (2)\n"""))
+
+    if m_in == "1":
         t_calc()
-    elif input == 2:
+    elif m_in == "2":
         tracker()
-    elif input == 3:
-        clock_in()
-    elif input == 4:
-        clock_out()
-    elif input == 5:
-        new_cut()
     else:
-        print("I'm sorry, please put in the number in integer format of the menu item you would like to execute: ")
+        print("Please put in the number of the menu item you would like to execute: ")
         menu()
 
 def tracker():
-    """As well as, info generator (quota/above or below, count and weight"""
-    weight_quota = 2000 #int in pounds
-    pieces_quota = 700 #pieces
-    meat = input(print("""Current progress: \n 
-    Total weight (1)\n
-    weight of each piece (2)
-    (1)\n
-    """))
-    meat.Capitalize()
-    meat_info = meats[meat]
-    if input == 1:
-        w = input(print("What's the total weight? "))
-        cut = input(print("What is the type of cut? "))
-        ans = w * cut
-        print(f"The total amount is: {ans}")
-    else input == 2:
-        d 
+    """info generator (quota/above or below, count and weight"""
+    def reach_goal(x,y): 
+        try: 
+            if x >= y:
+                print("Great job! You've met your goal!!")
+            elif x <= y:
+                print("Lets reach the goal tommorrow!")
+        except TypeError:
+            print("please input integer and try again")
+            tracker()
+    ed = input(print("Good job on perferming a hard days work! Please input your pounds per head (pph) of edible tallow for the day here: "))
+    reach_goal(int(ed),ed_g)
+    dt_ed.append(ed)    # add to list
+
+    id = input(print("That's great! Now please put your pounds per head (pph) of in-edible tallow for the day here: "))
+    reach_goal(int(id),id_g)
+    addrt(int(ed),int(id))
+    dt_id.append(id)    # add to list
+
+    display_charts()
+    menu()
 
 def t_calc():
     """Return a function that can calculate how long it's going to take"""
@@ -92,7 +129,70 @@ def t_calc():
             menu()
         else:
             t_calc()
-        
+
+
+def display_charts():
+    """chart #1: each day meat the goal, excess red or green"""
+    #pounds per head | edible talo 88 inedible 98 #2 tallo 8.8 greece 9 meat bone 10 blood 8.8 | tallos inventories every night 
+    # 288/hr 244.8mon.avg. 269chain speed actual 260, 299 chain speed actual 263 
+    # 265 harvestfloor
+    ar = np.array(dt_ed[-10:])
+    ar = ar.astype(float)
+    x = np.arange(len(ar))[::-1]
+    goal = ed_g * np.ones(len(ar))
+    goal2 = np.repeat(ed_g, len(ar))
+    plt.bar(x, ar,color="yellow")  # need to mark either up to 75 or up to n in ar
+    plt.bar(x, ar, bottom=goal, color='green')
+    dif = ar - goal2
+    dif2 = np.abs(dif)
+    negative_mask = dif < 0
+    plt.bar(x, dif2, bottom=ar, color='red',where=(negative_mask)) #need to find the goal - ar at those parts
+    plt.xlabel('Days')
+    plt.ylabel('Edible-Tallow')
+    plt.title('Past Previous 10 Days')
+    plt.legend()
+    plt.show()
+
+    # plt.fill_between(range(len(dt_ed)), rt_ed, ed_g, where=(rt_ed >= ed_g), color='green', alpha=0.2, label='Goal Met')
+    # plt.axhline(y=ed_g, color='r', linestyle='--', label='Daily Goal')
+    # plt.plot(dt_ed, cumulative_goal, color='r', linestyle='--', label='Daily Goal Accumulation')
+    # plt.legend()
+    # plt.grid(True)
+
+menu()
+
+"""
+    plt.figure(figsize=(8, 6))
+    plt.plot(rt_ed, label='Running Total')
+    plt.axhline(y=ed_g, color='r', linestyle='--', label='Daily Goal')
+    plt.axhline(y=ed_qg, color='g', linestyle='--', label='Quartly Goal')
+    plt.xlabel('Days')
+    plt.ylabel('Total')
+    plt.title('Running Total vs. Goals (Without Dates)')
+    plt.xticks(np.arange(len(rt_ed)))  # Set x-axis labels based on data length
+    plt.legend()
+    plt.grid(True)
+
+
+    # Plotting (without dates on x-axis)
+    plt.figure(figsize=(8, 6))
+    plt.plot(running_total, label='Running Total')
+    plt.axhline(y=daily_goal, color='r', linestyle='--', label='Daily Goal')
+    plt.axhline(y=annual_goal, color='g', linestyle='--', label='Annual Goal')
+
+    # Customize the plot (adjust x-axis labels if needed)
+    plt.xlabel('Days')
+    plt.ylabel('Total')
+    plt.title('Running Total vs. Goals (Without Dates)')
+    plt.xticks(np.arange(len(daily_totals)))  # Set x-axis labels based on data length
+    plt.legend()
+    plt.grid(True)
+    
+
+    plt.show()
+    """
+
+
 
 #     def number_of_slices(number_of_slices):
 #         """Calculates time it takes to cut"""
@@ -121,3 +221,4 @@ def t_calc():
 # print(f"Congratulations on using nic's software! to complete {number_of_cuts} {name_of_cut} cuts it would take {str(users_cut(number_of_cuts))} seconds. ")
 
 #Next, create a list for the users to pick and choose from | and make sure when they're about to pic, they know how long it takes to make the cut. 
+#inthe future could add date/time on graph in create a list to hold date/time. 
